b35b0cd1c0404c948a54d8e8c8dedf7814f16652
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2022-05-02 20:02:43 +0800

    Fix lines

--- goharveynormango.py
+++ goharveynormango.py
@@ -1,10 +1,6 @@
 import datetime
 import calendar
-
 today = datetime.date.today()
-
 nextday = today + datetime.timedelta(days=1)
-
 printdate = calendar.day_name[nextday.weekday()]
-
 print("Hurry! Sale must end this " + printdate + "!")

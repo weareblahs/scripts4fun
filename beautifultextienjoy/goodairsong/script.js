f48c0e2f869e2e3af31863f56c2fd487059d62c9
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2024-09-20 23:38:51 +0800

    +good air song

---
+++ beautifultextienjoy/goodairsong/script.js
@@ -0,0 +1,25 @@
+function english() {
+  title = "good air song";
+  author = "bun ten hiong";
+  introText =
+    "left over prison, north balcony, sit one soil room. room span eight feet, deep can four find. single shutter low small, white room short narrow, pollute down and ghost dark. this summer, air very jade: rain floom flaam floom four episode, float bed how many, this is water air; mud half season, steam very very the bad, this is soil air; deep fry sunrise very the hot, wind go four poop, this is sun air; roof glommy salary stove, help long hot torture, this is fire air; warehouse tofu deliver ton, tan tan force people, this is rice air; shoulder shoulder difficult return, ew ew sweat grime, this is human air. or toilet, or macabre, or dead rat, bad air spread spread, this is bad air. stack sereval air, that people umami no gau gau. and provide weakness, look up, already 2 year liao, thank god no sheep, is dead have nurture lah hor, then how nurture leh? hoh? Maangchi say: I good at nurture signal air. you have seven, I have one, use one defeat seven, how I get virus! furthermore, see sky ground very good write 'positive energy music'";
+  text =
+    "sky ground got good air, messy provide shape.\ndown is river hell, up is day star.\none person say signal air, good at stuck meditation.\nemperor road be clean aunt, swallow spit clear family.\nat time poor milk see, one one lean denmark green.\n\nat tidy too history simple, at quiet secretary fox pen.\nat clean&sup2; teoh leong zhui, at chinese soo wu jie.\nfor seriousness general military head, for funny samurai middle blood.\nfor teoh sui sun teeth, for color common mountain tongue.\nor for laos east hat, clean grass gao gao bingsu.\nor for go out teacher chart, ghost god cry very very de strong.\nor for cross river kak cik kak cik, generous swallow capricorn.\nor for attack thief beheading, reverse vertical head boom shakalaka.\n\nsai kei very pang bo, scales ten thousand ancient keeping without the book.\nwhen you get use to sun moon, live die peace foot debate.\nfloor maintain rely so standstill, sky pillar rely so respect.\nthree metals tie good life, tao justice for root.\n\nspit provide manhole sun nine, person who work super hard hard also honest no force.\nprecise jail baby's crown, pass car send poor north.\ndin tai fung pot sweet like taiwan, ask this cannot have.\ngloomy room stinky ghost fire, spring room boring sky #000000.\ncow horse same saddle, chicken wife phoenix eat.\none morning vantablack, spli as longkang middle spine.\nlike that winter have summer (vivaldi lu xiao eh har?) hundred sickness solo phobia easy.\ncut ah maggot like field, for my safe happy country.\nonly he very smart, yinyang no thieves.\n\ncare these flutes at, glance at floating white clouds.\ngentle gentle me heart sad, cang mang de tian ya thirsty have northern lights.\nphilosopher days gone far, classics at dusk.\nwindy curtains display book reading, old shine colors.";
+  htmlText = text.replace(/(?:\r\n|\r|\n)/g, "<br>");
+  finalArray = [title, author, introText, htmlText];
+  return finalArray;
+}
+
+function chinese() {
+  title = "正氣歌";
+  author = "宋 · 文天祥";
+  introText =
+    "余囚北庭，坐一土室。室廣八尺，深可四尋。單扉低小，白間短窄，汙下而幽暗。當此夏日，諸氣萃然：雨潦四集，浮動床幾，時則為水氣；塗泥半朝，蒸漚歷瀾，時則為土氣；乍晴暴熱，風道四塞，時則為日氣；簷陰薪爨，助長炎虐，時則為火氣；倉腐寄頓，陳陳逼人，時則為米氣；駢肩雜遝，腥臊汗垢，時則為人氣。或圊溷、或毀屍、或腐鼠，惡氣雜出，時則為穢氣。疊是數氣，當之者鮮不為厲。而予以孱弱，俯仰其間，於茲二年矣，幸而無恙，是殆有養致然爾。然亦安知所養何哉？孟子曰：吾善養吾浩然之氣。彼氣有七，吾氣有一，以一敵七，吾何患焉！況浩然者，乃天地之正氣也，作正氣歌一首。";
+  text =
+    "天地有正氣，雜然賦流形。\n下則為河嶽，上則為日星。\n於人曰浩然，沛乎塞蒼冥。\n皇路當清夷，含和吐明庭。\n時窮節乃見，一一垂丹青。\n\n在齊太史簡，在晉董狐筆。\n在秦張良椎，在漢蘇武節。\n為嚴將軍頭，為嵇侍中血。\n為張睢陽齒，為顏常山舌。\n或為遼東帽，清操厲冰雪。\n或為出師表，鬼神泣壯烈。\n或為渡江楫，慷慨吞胡羯。\n或為擊賊笏，逆豎頭破裂。\n\n是氣所磅礡，凜烈萬古存。\n當其貫日月，生死安足論。\n地維賴以立，天柱賴以尊。\n三綱實繫命，道義為之根。\n\n嗟予遘陽九，隸也實不力。\n楚囚纓其冠，傳車送窮北。\n鼎鑊甘如飴，求之不可得。\n陰房闃鬼火，春院閟天黑。\n牛驥同一皁，雞棲鳳凰食。\n\n一朝蒙霧露，分作溝中瘠。\n如此再寒暑，百沴自辟易。\n嗟哉沮洳場，為我安樂國。\n豈有他繆巧，陰陽不能賊。\n\n顧此耿耿在，仰視浮雲白。\n悠悠我心悲，蒼天曷有極。\n哲人日已遠，典刑在夙昔。\n風簷展書讀，古道照顏色。";
+  htmlText = text.replace(/(?:\r\n|\r|\n)/g, "<br>");
+  finalArray = [title, author, introText, htmlText];
+  return finalArray;
+}
+
+console.log(chinese());
